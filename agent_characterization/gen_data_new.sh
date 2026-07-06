1bb0d1248cf8afd2b0ff2d25083f593b3ed59661
jiatianxiao2012 <jiatianxiao2012@gmail.com>
2026-07-05 20:07:38 -0400

    Update gen_data_new.sh

--- agent_characterization/gen_data_new.sh
+++ agent_characterization/gen_data_new.sh
@@ -132,11 +132,11 @@ RING_POLICIES=(
   "11" "25" "4" "22" "6" "18" "13" "15"
 )
 
-LAYOUT="hallway"
-POP_DIR="$HALLWAY_POP_DIR"
+LAYOUT="fc"
+POP_DIR="$FC_POP_DIR"
 YAML="${YAML_CONFIG}_${LAYOUT}.yaml"
 
-for POL in "${HALLWAY_POLICIES[@]}"; do
+for POL in "${FC_POLICIES[@]}"; do
   echo "Population Directory: $POP_DIR"
   echo "Processing pair: pol$POL vs polBR$POL"
   echo "Using Config: $YAML"
